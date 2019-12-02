d23b193eaa98b9889061dac83edff7a5ca19be64
Mirel Home <mirelalex2094@gmail.com>
2019-12-02 19:43:04 +0200

    creating, getting and populating reviews and virtual populate the tours and reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -109,6 +109,12 @@ const tourSchema = new mongoose.Schema(
         ref: 'User'
       }
     ]
+    // reviews: [
+    //   {
+    //     type: mongoose.Schema.ObjectId,
+    //     ref: 'Review'
+    //   }
+    // ]
   },
   {
     toJSON: { virtuals: true },
@@ -118,6 +124,14 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
+});
+
+// Virtual populate
+
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id'
 });
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
