ac48bac97570ce3f137944298e9751e2fb729665
Mirel Home <mirelalex2094@gmail.com>
2019-12-05 21:02:27 +0200

    geospatial aggregation - calculating distances and postman api documentation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -192,6 +192,8 @@ exports.getDistances = catchAsync(async (req, res, next) => {
   const { latlng, unit } = req.params;
   const [lat, lng] = latlng.split(',');
 
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
   if (!lat || !lng) {
     next(
       new AppError(
@@ -203,8 +205,26 @@ exports.getDistances = catchAsync(async (req, res, next) => {
   const distances = await Tour.aggregate([
     {
       $geoNear: {
-        // near:
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1]
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier
+      }
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1
       }
     }
   ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances
+    }
+  });
 });
