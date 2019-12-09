ef410dc7747ef43616855cc46bedd90c40642cb3
Mirel Home <mirelalex2094@gmail.com>
2019-12-09 21:07:41 +0200

    building the login screen

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -15,7 +15,7 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1. Get the date for the requested tour, includins reviews and guides
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
@@ -30,3 +30,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour
   });
 });
+
+exports.getLoginForm = (req, res, next) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
