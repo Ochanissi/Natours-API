69a93fce834ec4da8132095aa82a0159d95252dd
Mirel Home <mirelalex2094@gmail.com>
2019-12-09 20:58:53 +0200

    integrating mapbox and building the map

---
+++ public/js/mapbox.js
@@ -0,0 +1,53 @@
+/* eslint-disable */
+
+console.log('Hello from the client side!');
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoib2NoYW5pc3NpIiwiYSI6ImNrM3lycWVmMzBvMHAzbHA2NWw4MHF5OWoifQ.jUok_FWVOlBb_7866W6pyA';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/ochanissi/ck3yrv7ik0hij1cnz20qas2x6',
+  scrollZoom: false
+  //   center: [25.781842, -80.128473],
+  //   zoom: 10,
+  //   interactive: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom'
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p> ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend the map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100
+  }
+});
